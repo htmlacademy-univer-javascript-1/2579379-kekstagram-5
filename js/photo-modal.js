1dc40c3cc5e9f9354c9325dee12752abb17cbb9c
ValentinaKobtseva <kvaaa0601@gmail.com>
2024-12-18 13:23:13 +0700

    Открывается и закрывается (часть 2)
    Добавляет загрузку 5 комментариев при клике по кнопке "Загрузить ещё"

--- js/photo-modal.js
+++ js/photo-modal.js
@@ -1,14 +1,11 @@
 import { allPhotosContainer, allPhotos } from "./draw.js";
 import { isEscKey } from "./utils.js";
 import { renderBigPhoto } from "./render-full-size-photo.js";
-import { renderComments, cleanComments } from "./render-comments.js";
+import { onCommentsLoaderClick, cleanComments } from "./render-comments.js";
 
 const bigPhotoContainer = document.querySelector(".big-picture");
 const closeBigPhotoButton = document.querySelector(".big-picture__cancel");
 const body = document.querySelector("body");
-
-const commentsCount = document.querySelector(".social__comment-count");
-const commentsLoader = document.querySelector(".comments-loader");
 
 const closeOnEscape = (event) => {
   if (isEscKey(event)) {
@@ -35,16 +32,13 @@ const openPhotoModal = (photoId) => {
   document.addEventListener("keydown", closeOnEscape);
 
   const currentPhoto = allPhotos.find((photo) => photo.id === +photoId);
-  const currentPhotoComments = currentPhoto.comments;
 
   bigPhotoContainer.classList.remove("hidden");
 
   renderBigPhoto(currentPhoto);
-  renderComments(currentPhotoComments);
+  onCommentsLoaderClick(currentPhoto);
 
   body.classList.add(".modal-open");
-  //commentsCount.classList.add("hidden");
-  //commentsLoader.classList.add("hidden");
 };
 
 const handlePhotoClick = () => {
